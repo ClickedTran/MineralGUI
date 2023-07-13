755c681c916d9e1a777c5c5451de6e8b1225a0fd
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-07-13 20:49:51 +0700

    Update and Clean EventListener.php

--- src/ClickedTran/mineral/EventListener.php
+++ src/ClickedTran/mineral/EventListener.php
@@ -58,116 +58,37 @@ class EventListener implements Listener{
       if($player->isCreative(true)){
          return;
       }else{
-        /* LAPIZ */
-        switch($block->getTypeId()){
-         case BlockTypeIds::LAPIS_LAZULI_ORE:
-          $event->setDrops(array());
-          $id = mt_rand(1, $level);
-          $this->data->set(Mineral::LAPIS, ($this->data->get(Mineral::LAPIS) + ($id * 4)));
-          $this->data->save();
-         break;
-        /* LAPIZ BLOCK */
-         case BlockTypeIds::LAPIS_LAZULI:
-           $event->setDrops(array());
-           $this->data->set(Mineral::LAPIS_BLOCK, ($this->data->get(Mineral::LAPIS_BLOCK) + 1));
-           $this->data->save();
-         break;
-        /* COAL */
-         case BlockTypeIds::COAL_ORE:
-           $event->setDrops(array());
-           $id = mt_rand(1, $level);
-           $this->data->set(Mineral::COAL, ($this->data->get(Mineral::COAL) + ($id * 4)));
-           $this->data->save();
-         break;
-        /* COAL BLOCK */
-         case BlockTypeIds::COAL:
-           $event->setDrops(array());
-           $this->data->set(Mineral::COAL_BLOCK, ($this->data->get(Mineral::COAL_BLOCK) + 1));
-           $this->data->save();
-         break;
-        /* IRON ORE */ 
-         case BlockTypeIds::IRON_ORE:
-          $event->setDrops(array());
-          $id = mt_rand(1, $level);
-          $this->data->set(Mineral::IRON_ORE, ($this->data->get(Mineral::IRON_ORE) + $id));
-          //I DON'T USE IRON INGOT BECAUSE THAT IS AUTO SMELTING
-          $this->data->save();
-         break;
-        /* IRON BLOCK */
-         case BlockTypeIds::IRON:
-           $event->setDrops(array());
-           $this->data->set(Mineral::IRON_BLOCK, ($this->data->get(Mineral::IRON_BLOCK) + 1));
-           $this->data->save();
-         break;
-        /* GOLD ORE */
-         case BlockTypeIds::GOLD_ORE:
-           $event->setDrops(array());
-           $id = mt_rand(1, $level);
-           $this->data->set(Mineral::GOLD_ORE, ($this->data->get(Mineral::GOLD_ORE) + $id));
-           //I DON'T USE GOLD INGOT BECAUSE THAT IS AUTO SMELTING
-           $this->data->save();
-         break;
-        /* GOLD BLOCK */
-         case BlockTypeIds::GOLD:
-           $event->setDrops(array());
-           $this->data->set(Mineral::GOLD_BLOCK, ($this->data->get(Mineral::GOLD_BLOCK) + 1));
-           $this->data->save();
-         break;
-        /* REDSTONE */
-         case BlockTypeIds::REDSTONE_ORE:
-           $event->setDrops(array());
-           $id = mt_rand(1, $level);
-           $this->data->set(Mineral::REDSTONE, ($this->data->get(Mineral::REDSTONE) + ($id * 4)));
-           $this->data->save();
-         break;
-        /* REDSTONE BLOCK */
-         case BlockTypeIds::REDSTONE:
-           $event->setDrops(array());
-           $this->data->set(Mineral::REDSTONE_BLOCK, ($this->data->get(Mineral::REDSTONE_BLOCK) +1));
-           $this->data->save();
-         break;
-        /* DIAMOND */
-         case BlockTypeIds::DIAMOND_ORE:
-           $event->setDrops(array());
-           $id = mt_rand(1, $level);
-           $this->data->set(Mineral::DIAMOND, ($this->data->get(Mineral::DIAMOND) + $id));
-           $this->data->save();
-         break;
-        /* DIAMOND BLOCK */
-         case BlockTypeIds::DIAMOND:
-          $event->setDrops(array());
-          $this->data->set(Mineral::DIAMOND_BLOCK, ($this->data->get(Mineral::DIAMOND_BLOCK) + 1));
-          $this->data->save();
-         break;
-        /* EMERALD */
-         case BlockTypeIds::EMERALD_ORE:
-           $event->setDrops(array());
-           $id = mt_rand(1, $level);
-           $this->data->set(Mineral::EMERALD, ($this->data->get(Mineral::EMERALD) + $id));
-           $this->data->save();
-         break;
-        /* EMERALD BLOCK */
-         case BlockTypeIds::EMERALD:
-           $event->setDrops(array());
-           $this->data->set(Mineral::EMERALD_BLOCK, ($this->data->get(Mineral::EMERALD_BLOCK) + 1));
-           $this->data->save();
-         break;
-        /* COBBLESTONE */
-         case BlockTypeIds::COBBLESTONE:
-           $event->setDrops(array());
-           $this->data->set(Mineral::COBBLESTONE, ($this->data->get(Mineral::COBBLESTONE) + 1));
-           $this->data->save();
-         break;
-        /* STONE BUT AFTER BREAK IS COBBLESTONE */
-         case BlockTypeIds::STONE:
-           $event->setDrops(array());
-           $this->data->set(Mineral::COBBLESTONE, ($this->data->get(Mineral::COBBLESTONE) + 1));
-           $this->data->save();
-        break;
-       } 
-      }
+        $blocks = [
+          0 => ["type" => BlockTypeIds::LAPIS_LAZULI_ORE, "random" => (mt_rand(1, $level) * 4), "mineral" => Mineral::LAPIS],
+          1 => ["type" => BlockTypeIds::COAL_ORE, "random" => (mt_rand(1, $level) * 4), "mineral" => Mineral::COAL],
+          2 => ["type" => BlockTypeIds::REDSTONE_ORE, "random" => (mt_rand(1, $level) * 4), "mineral" => Mineral::REDSTONE],
+          3 => ["type" => BlockTypeIds::IRON_ORE, "random" => mt_rand(1, $level), "mineral" => Mineral::IRON_ORE],
+          4 => ["type" => BlockTypeIds::GOLD_ORE, "random" => mt_rand(1, $level), "mineral" => Mineral::GOLD_ORE],
+          5 => ["type" => BlockTypeIds::DIAMOND_ORE, "random" => mt_rand(1, $level), "mineral" => Mineral::DIAMOND],
+          6 => ["type" => BlockTypeIds::EMERALD_ORE, "random" => mt_rand(1, $level), "mineral" => Mineral::EMERALD],
+          7 => ["type" => BlockTypeIds::LAPIS_LAZULI, "random" => 1, "mineral" => Mineral::LAPIS_BLOCK],
+          8 => ["type" => BlockTypeIds::REDSTONE, "random" => 1, "mineral" => Mineral::REDSTONE_BLOCK],
+          9 => ["type" => BlockTypeIds::COAL, "random" => 1, "mineral" => Mineral::COAL_BLOCK],
+          10 => ["type" => BlockTypeIds::IRON, "random" => 1, "mineral" => Mineral::IRON_BLOCK],
+          11 => ["type" => BlockTypeIds::GOLD, "random" => 1, "mineral" => Mineral::GOLD_BLOCK],
+          12 => ["type" => BlockTypeIds::DIAMOND, "random" => 1, "mineral" => Mineral::DIAMOND_BLOCK],
+          13 => ["type" => BlockTypeIds::EMERALD, "random" => 1, "mineral" => Mineral::EMERALD_BLOCK],
+          14 => ["type" => BlockTypeIds::COBBLESTONE, "random" => mt_rand(1, $level), "mineral" => Mineral::COBBLESTONE],
+          15 => ["type" => BlockTypeIds::STONE, "random" => mt_rand(1, $level), "mineral" => Mineral::COBBLESTONE]
+          ];
+        foreach($blocks as $slot => $blockData){
+          $type = $blockData["type"];
+          $random = $blockData["random"];
+          $mineral = $blockData["mineral"];
+          if($block->getTypeId() === $type){
+             $event->setDrops([]);
+             $this->data->set($mineral, ($this->data->get($mineral) + $random));
+             $this->data->save();
+          }
+         }
+       }
      }
-    }
+   }
   }
     
     public function onJoin(PlayerJoinEvent $event): void{
